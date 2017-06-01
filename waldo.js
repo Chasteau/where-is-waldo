534bcccd70f9ee1b41d64bf5550ba358da1347a2
Chasteau <davidchasteau@gmail.com>
2017-06-01 16:24:11 +0000

    v2 waldo

--- waldo.js
+++ waldo.js
@@ -1,20 +1,5 @@
-// var myFn = function() {
-//   console.log("I am function.");
-// }
-// console.log(myFn);
 
-// myFn.someAttribute = 42;
-// //console.log(myFn.someAttribute);
-
-// console.log(myFn);
-
-// function runner(f) {
-//   f();
-// }
-
-// runner(myFn);
-
-// The second argument/parameter is expected to be a function
+// v2 waldo
 
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
